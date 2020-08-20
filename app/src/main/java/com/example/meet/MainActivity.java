b39f087f640583aba5fac4f124efb3f5a1535b4a
Nii <ticialjc@gmail.com>
2020-08-20 10:22:45 +0800

    add AppCenter SDK

--- app/src/main/java/com/example/meet/MainActivity.java
+++ app/src/main/java/com/example/meet/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!AppCenter.isConfigured()){
+            AppCenter.start(getApplication(), "464aa4d5-2543-4882-9696-0c3e51e15911", Analytics.class, Crashes.class);
+        }
     }
 }
